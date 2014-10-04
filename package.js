51ec3418c133bc3bbf33b21b614eb1d5cd9628b7
Tim Brandin <tim.brandin@gmail.com>
2014-10-04 23:32:19 +0200

    Added tests.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Logging for packages, show where in the application code something caused the logging.",
-  version: "1.0.3",
+  version: "1.0.4",
   git: "https://github.com/timbrandin/meteor-package-logging"
 });
 
@@ -13,4 +13,11 @@ Package.onUse(function(api) {
   api.export('PLog');
 
   api.addFiles('timbrandin:package-logging.js');
+});
+
+Package.onTest(function (api) {
+  api.use(['tinytest', 'test-helpers'], ['client', 'server']);
+  api.use('timbrandin:package-logging', ['client', 'server']);
+
+  api.add_files('timbrandin:package-logging_tests.js', ['client', 'server']);
 });
